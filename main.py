0bc7f72f0275493c8714f9023a7c5e211c332aa6
Spirit <79371068+SpiritGreen@users.noreply.github.com>
2023-05-23 20:04:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+print('Hello Replit')
+print()
+print("Well we")
+print("just use more lines")
+print("of code")
+print()
+print("""Anything that starts
+with three quotes, and ends
+in three quotes can span
+many lines and even contain " symbols
+within it without freaking anything out!""")
+print()
+print('= MUSIC+ =')
+print("> Songs")
+print("> Albums")
+print("> Artists")
+print()
+print('''Spirit Green    28.01.2023
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+I am feeling 🐝
+You can follow my progress at https://replit.com/@SpiritGreen''')
